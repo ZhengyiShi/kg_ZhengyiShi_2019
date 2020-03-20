e39acd2b0c2c1fa9336b64a4faf103644e60901d
Zhengyi Shi <shizy628@gmail.com>
2020-03-20 11:17:43 -0700

    Add some comments.

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import sys
 
 
 def check_mapping(str1, str2):
+    # if length of two strings are different, then one-to-one mapping doesn't existed.
     if len(str1) != len(str2):
         return False
-
+    
+    # We can create a map using dict or list.
+    # list costs less memory, but also costs more time with the increasing length of input string.
+    # dict costs more memory, but are more efficient.
     map1 = {}
     for i in range(len(str1)):
         val = map1.get(str1[i], '0')
